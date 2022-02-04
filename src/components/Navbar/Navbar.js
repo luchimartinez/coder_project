509f313a110fc0371068bba4d5b0a3512177a964
Lucia <luchi15.8.lm@gmail.com>
2022-02-03 21:33:34 -0300

    Nav bar improvements

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+      <nav className = 'Navbar'>
+          <div>
+              <h4>Pokemon Store</h4>
+              <p>Pokebolls</p>
+              <p>Medicines</p>
+              <p>Other objects</p>
+
+          </div>
+      </nav>
+    )
+  }
+
+  export default Navbar
